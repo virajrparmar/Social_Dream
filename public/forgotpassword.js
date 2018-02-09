7c62ce76489c535a0dd264e62d6ff8508749e42c
Viraj Parmar <virajparmar98@gmail.com>
2018-02-09 15:57:02 +0530

    v

--- public/forgotpassword.js
+++ public/forgotpassword.js
@@ -9,5 +9,64 @@ function ValidateForm() {
 		alert("Invalid Mobile Number!");
 		return false;
 	}
-	return true;
+
+	var email = document.getElementById('email').value;
+	var mobno = document.getElementById('mobno').value;
+
+	var db = firebase.firestore();
+
+	var docRef = db.collection("Users").doc(email);
+
+	var otp = Math.floor(Math.random() * (9999 - 1000 + 1) ) + 1000;
+
+						docRef.update({
+						    FP_OTP: otp,
+						    FP_OTP_Verified: false
+						})
+						.then(function() {
+						    console.log("Document successfully updated!");
+						})
+						.catch(function(error) {
+						    // The document probably doesn't exist.
+						    console.error("Error updating document: ", error);
+						});
+
+	docRef.get().then(function(doc) {
+
+			    if (doc.exists) {
+
+			    	if(doc.data().Mobile == mobno) {
+
+						var auth = firebase.auth();
+
+						auth.sendPasswordResetEmail(email).then(function() {
+						  console.log("Email Sent");
+						}).catch(function(error) {
+						  console.log("An error happened");
+						});
+
+						alert('Account matched!');
+
+			    		window.location.href = "forgotpasswordotp.html"
+
+			    	} else {
+
+			    		alert("Wrong E-mail ID or Mobile Number!");
+			        	document.getElementById('email').value = "";
+			        	document.getElementById('mobno').value = "";
+
+			    	}
+
+			    } else {
+
+			        alert("E-mail ID does not exist!");
+			        document.getElementById('email').value = "";
+			        document.getElementById('mobno').value = "";
+			        
+			    }
+			}).catch(function(error) {
+			    console.log("Error getting document:", error);
+			});
+
+	return false;
 }
